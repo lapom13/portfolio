85d616ab9f6dbd4b6997be119805a5416727ad50
Oleg Ogorodnikov <Lapom_13@mail.ru>
2018-06-24 15:41:08 +0300

    Сделан адаптив страницы Обо мне

--- src/assets/scripts/modules/flipper.js
+++ src/assets/scripts/modules/flipper.js
@@ -1,18 +1,27 @@
 
 const authBtn = document.querySelector('.auth-btn');
-const flipper = document.querySelector('.flipper');
-const wrapper = document.querySelector('.wrapper');
+const toMainBtn = document.querySelector(".btn-main");
+const flipper = document.querySelector(".flipper");
+const wrapper = document.querySelector(".wrapper");
 
-authBtn.addEventListener ('click', function (event) {
-    flipper.style.transform = "rotateY(180deg)";
-    authBtn.style.display = "none";
+authBtn.addEventListener("click", function(event) {
+  flipper.style.transform = "rotateY(180deg)";
+  authBtn.style.display = "none";
 });
 
-wrapper.addEventListener ('click', function (event) {
+toMainBtn.addEventListener("click", function(event) {
+    flipper.style.transform = "rotateY(0deg)";
+    authBtn.style.display = "";
+});
+
+wrapper.addEventListener("click", function(event) {
     let target = event.target;
     if (target == wrapper) {
         flipper.style.transform = "rotateY(0deg)";
         authBtn.style.display = "";
     }
 });
+
+
+
 
