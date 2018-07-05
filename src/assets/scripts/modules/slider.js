79c65671e9bbf96f1e9fe68b0884861b9262142d
Oleg Ogorodnikov <Lapom_13@mail.ru>
2018-07-05 17:07:52 +0300

    Сделан display у слайдера

--- src/assets/scripts/modules/slider.js
+++ src/assets/scripts/modules/slider.js
@@ -16,8 +16,29 @@ const display = {
 
 const buttons = {
     template: "#slider-buttons",
+    methods: {
+        slide(direction) {
+            this.$emit("slide", direction)
+        },
+        getReqImage(direction) {
+            const worksArray = [...this.works];
+
+            switch(direction) {
+                case 'prev' :
+                    const lastItem = worksArray[worksArray.length - 1];
+                    worksArray.unshift(lastItem);
+                    worksArray.pop();
+                    break;
+                case 'next':
+                    worksArray.push(worksArray[0]);
+                    worksArray.shift();
+                    break;
+            }
+        }
+    },
     props: {
-        works: Array
+        works: Array,
+        currentIndex: Number
     }
 };
 
@@ -30,11 +51,36 @@ new Vue({
     },
     data: {
         works: [],
-        currentWork: []
+        currentWork: [],
+        currentIndex: 0
     },
     created() {
         this.works = require("../../../../data/works.json");
         this.currentWork = this.works[0];
+    },
+    watch: {
+        currentIndex(value) {
+            const workAmount = this.works.length - 1;
+            if(value > workAmount) this.currentIndex = 0;
+            if(value < 0) this.currentIndex = workAmount;
+            
+            this.currentWork = this.works[value];
+            console.log(value);
+        }
+    },
+    methods: {
+        handleSlide(direction) {
+            console.log(direction);
+            switch(direction) {
+                case 'prev' :
+                    this.currentIndex = this.currentIndex - 1;
+                    break;
+                
+                case 'next':
+                    this.currentIndex = this.currentIndex + 1;
+                    break;
+            }
+        }
     },
     template: "#slider",
 });
